27f7ed050823adfad5aca9e045b68efcc9db6a4a
Alon Gubkin <alon.gubkin@gmail.com>
2013-01-27 21:56:42 +0200

    Fixed color in trails.sql.

--- trails.sql
+++ trails.sql
@@ -4,4 +4,4 @@ INSERT INTO `store_categories` (`display_name`, `description`, `require_plugin`,
 set @category_id = (select last_insert_id());
 
 INSERT INTO `store_items` (`name`, `display_name`, `description`, `type`, `loadout_slot`, `price`, `category_id`, `attrs`) VALUES
-('cocacola', 'Coca Cola', NULL, 'trails', 'trails', 250, @category_id, '{ "material": "materials/sprites/trails/cocacola.vmt", "color": "FFFFFF", "startingwidth": 10 }');
+('cocacola', 'Coca Cola', NULL, 'trails', 'trails', 250, @category_id, '{ "material": "materials/sprites/trails/cocacola.vmt", "color": "255 255 255", "startingwidth": 10 }');
